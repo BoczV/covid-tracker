37dd663d1a1e6009ee449b6871767c8faaf2ac67
Viktor Bocz <beviktor95@gmail.com>
2020-08-01 17:37:29 +0200

    Select drop down with countries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./Styles/App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import Axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  // useEffect(() => {
+  //   const url = "https://disease.sh/v3/covid-19/countries";
+  //   const getCountriesData = async () => {
+  //     await fetch(url)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const countries = data.map((country) => ({
+  //           name: country.country,
+  //           value: country.countryInfo.iso2,
+  //           id: country.countryInfo._id,
+  //         }));
+  //         setCountries(countries);
+  //       });
+  //   };
+  //   getCountriesData();
+  // }, []);
+
+  useEffect(() => {
+    const url = "https://disease.sh/v3/covid-19/countries";
+    Axios.get(url).then((res) => {
+      const countries = res.data.map((country) => ({
+        name: country.country,
+        value: country.countryInfo.iso2,
+        id: country.countryInfo._id,
+      }));
+      setCountries(countries);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem key={country.id} value={country.value}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Titlte + Select input */}
+      {/* Infoboxs */}
+      {/* Infoboxs */}
+      {/* Infoboxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
